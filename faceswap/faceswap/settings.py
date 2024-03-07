667713b3d84ce80eff3617502ac1f6b4e3dbd811
v.kovalchuk <v.kovalchuk@crysto.ru>
2024-03-07 22:01:05 +0300

    some refactoring

--- faceswap/faceswap/settings.py
+++ faceswap/faceswap/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "api/static/"
+STATIC_URL = "/api/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
@@ -143,7 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/api/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
--- faceswap/faceswap/urls.py
+++ faceswap/faceswap/urls.py
@@ -18,22 +18,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.static import serve
+from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("", include("mainapp.urls")),
     path("api/", include("api.urls")),
-    re_path(
-        r"^outputs/(?P<path>.*)$",
-        serve,
-        {
-            "document_root": settings.BASE_DIR / "mainapp/outputs",
-        },
-        name="serve_video",
-    ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(
