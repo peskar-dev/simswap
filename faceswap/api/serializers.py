58583b674da0d10b544bf4c05cd7d92b1c45166a
v.kovalchuk <v.kovalchuk@crysto.ru>
2024-03-14 22:14:43 +0300

    Serialization debug

--- faceswap/api/serializers.py
+++ faceswap/api/serializers.py
@@ -5,8 +5,13 @@ class ImageUploadSerializer(serializers.Serializer):
     file = serializers.ImageField()
 
     def validate_file(self, value):
-        if value.image.format.lower() not in ["png", "jpeg", "jpg"]:
+        if (image_format := value.image.format.lower()) not in [
+            "png",
+            "jpeg",
+            "jpg",
+        ]:
             raise serializers.ValidationError(
-                "Invalid file format. Only PNG, JPEG and JPG are supported."
+                f"Invalid file format. Only PNG, JPEG and JPG are supported. "
+                f"You specified: {image_format}"
             )
         return value
