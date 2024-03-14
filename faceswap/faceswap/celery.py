4db6ce9b95ef085db08ef7dac850f0bb13be5a03
v.kovalchuk <v.kovalchuk@crysto.ru>
2024-03-14 05:48:51 +0300

    Add delete worker

--- faceswap/faceswap/celery.py
+++ faceswap/faceswap/celery.py
@@ -7,3 +7,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "faceswap.settings")
 app = Celery("faceswap")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+app.conf.task_routes = {
+    "api.tasks.delete_dir": {"queue": "delete"},
+    "api.tasks.generate_faceswap": {"queue": "generate"},
+}
